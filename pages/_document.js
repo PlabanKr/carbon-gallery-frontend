8c953eb332491909410a9d0bcba15c7a6b184e72
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-21 19:42:39 +0530

    Add CDN for bootstrap and fontawesome

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,28 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/icon.jpg"></link>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <meta name="theme-color" content="#fff" />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+            crossOrigin="anonymous"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+            crossOrigin="anonymous"
+            defer
+          ></script>
         </body>
       </Html>
     );
