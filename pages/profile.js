373e8a0c4a526094fd03946f1a573abbf64961bc
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-21 19:06:53 +0530

    Add demo contents in login, signup, profile page

---
+++ pages/profile.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return (
+      <>
+        <div>Carbon Gallery Profile page</div>
+      </>
+    );
+  }
+  
---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+export default function SignUp() {
+    return (
+      <>
+        <div>Carbon Gallery SignUp Page</div>
+      </>
+    );
+  }
+  
