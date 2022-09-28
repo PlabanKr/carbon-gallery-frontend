7f47babb5d0e2fe9980aee3e46d5f01255815fb8
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-28 12:46:24 +0530

    Add login page html css

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,49 @@
+import Link from "next/link";
+import Sidebar from "../components/sidebar";
+import Topbar from "../components/topbar";
+import styles from "../styles/Login.module.css";
+
 export default function Login() {
   return (
     <>
-      <div>Carbon Gallery Login Page</div>
+      <Topbar />
+      <Sidebar />
+      <div className={styles.mainSection}>
+        <div className={styles.center}>
+          <h1>Create Account</h1>
+          <form>
+            <div className={styles.textField}>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                required
+                autoComplete="off"
+              />
+              <span></span>
+              <label htmlFor="username">UserName</label>
+            </div>
+            <div className={styles.textField}>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                autoComplete="nope"
+              />
+              <span></span>
+              <label htmlFor="password">Password</label>
+            </div>
+            <input type="submit" value="Login" className={styles.submitBtn} />
+            <div className={styles.signupLink}>
+              Don&apos;t have an account?{" "}
+              <Link href="/signup">
+                <a>Signup</a>
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
