857b6f6dd80a07e37cad37cdff8f107988e0261c
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-09-29 09:45:29 +0530

    Add upload functionality

--- components/topbar/index.js
+++ components/topbar/index.js
@@ -1,9 +1,11 @@
+import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/icons/logo.png";
 import styles from "../../styles/Topbar.module.css";
 
 const Topbar = () => {
+  const router = useRouter();
   return (
     <nav
       className="navbar navbar-expand-lg bg-light fixed-top"
@@ -28,16 +30,26 @@ const Topbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="nav navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link href="/login">
-                <a className="btn btn-outline-secondary me-2">Login</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/signup">
-                <a className="btn btn-outline-primary">Signup</a>
-              </Link>
-            </li>
+            {router.pathname != "/profile" ? (
+              <>
+                <li className="nav-item">
+                  <Link href="/login">
+                    <a className="btn btn-outline-secondary me-2">Login</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/signup">
+                    <a className="btn btn-outline-primary">Signup</a>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li className="nav-item">
+                <Link href="/">
+                  <a className="btn btn-outline-danger">Logout</a>
+                </Link>
+              </li>
+            )}
           </ul>
         </div>
       </div>
